898b72e83e8991baf82535db5d99694c39163113
Bonggt <kepy1106@gmail.com>
2019-03-20 00:20:54 +0900

    Express(1)

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Express Server is running on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
