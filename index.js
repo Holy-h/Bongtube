04e26dca49b15951abd0f7190fb264b32799309d
Bonggt <kepy1106@gmail.com>
2019-03-20 23:24:35 +0900

    Express

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function handleListening() {
   console.log(`Express Server is running on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("I am Home");
+}
+
+function handleProfile(req, res) {
+  res.send("I am Profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
